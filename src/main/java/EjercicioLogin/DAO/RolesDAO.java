e31c328d7821d7c2330e0d239ef2999601d607ae
srfzm <saulrefoyo@gmail.com>
2021-10-28 12:41:42 +0200

    Creado el DAO de roles y añadidas las funcionalidades de insertar y obtener lista de roles.

---
+++ src/main/java/EjercicioLogin/DAO/RolesDAO.java
@@ -0,0 +1,23 @@
+package EjercicioLogin.DAO;
+
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import EjercicioLogin.Modelos.Roles;
+import EjercicioLogin.Utilidades.HibernateUtil;
+
+public class RolesDAO {
+
+	public static void insertRoles(Session s, Roles roles) {
+		s.save(roles);
+		HibernateUtil.logger.info("Realizada inserccion.");
+	}
+	
+	public static ArrayList<Roles> getLista(Session s) {
+		String hql= "from Roles";
+		Query query = s.createQuery(hql);
+		return (ArrayList<Roles>) query.list();
+	}
+}
