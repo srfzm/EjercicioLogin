dceb64b9e9d98976b6e8e853ed483da450f21372
Nombre <prueba@prueba.com>
2021-10-28 20:51:08 +0200

    Añadido FiltroSesion.java para filtrar la sesion y no permitir aceder si no se esta logueado.

---
+++ src/main/java/EjercicioLogin/Filtros/FiltroSesion.java
@@ -0,0 +1,57 @@
+package EjercicioLogin.Filtros;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class FiltroSesion
+ */
+@WebFilter("/*")
+public class FiltroSesion implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public FiltroSesion() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		HttpServletRequest req = (HttpServletRequest)request;
+		// pass the request along the filter chain
+		HttpSession sesion = req.getSession(false);
+		if(sesion!=null || req.getRequestURI().endsWith("Login"))
+			chain.doFilter(request, response);
+		else
+			((HttpServletResponse)response).sendRedirect("login.html");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
