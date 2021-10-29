18f3e0e6290026b1d5f391114b7918dd72ca71c9
Nombre <prueba@prueba.com>
2021-10-29 19:40:05 +0200

    Creados los filtro de control para el servlet y jsp de creacion de categorias.

---
+++ src/main/java/EjercicioLogin/Filtros/FiltroControlJSPCategoria.java
@@ -0,0 +1,61 @@
+package EjercicioLogin.Filtros;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import EjercicioLogin.Modelos.Usuarios;
+import EjercicioLogin.Utilidades.HibernateUtil;
+
+/**
+ * Servlet Filter implementation class FiltroControlJSPCategoria
+ */
+@WebFilter(filterName="FiltroControlJSPCategoria")
+public class FiltroControlJSPCategoria implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public FiltroControlJSPCategoria() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		HibernateUtil.logger.info("Accediendo al filtro de control del jsp de crear categorias.");
+		HttpServletRequest req = (HttpServletRequest)request;
+		// pass the request along the filter chain
+		HttpSession sesion = req.getSession(false);
+		if(((Usuarios)sesion.getAttribute("usuario")).getId_rol()==1)
+			chain.doFilter(request, response);
+		else
+			req.getRequestDispatcher("menu.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
