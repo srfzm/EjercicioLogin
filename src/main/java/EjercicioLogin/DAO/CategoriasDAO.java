9da76cbdb26740043fa73d7d60d8bfc722310998
Nombre <prueba@prueba.com>
2021-10-29 18:29:00 +0200

    Creado DAO para Categorias, junto con los metodos de insertar y listar.

---
+++ src/main/java/EjercicioLogin/DAO/CategoriasDAO.java
@@ -0,0 +1,23 @@
+package EjercicioLogin.DAO;
+
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import EjercicioLogin.Modelos.Categorias;
+import EjercicioLogin.Utilidades.HibernateUtil;
+
+public class CategoriasDAO {
+
+	public static void insertRoles(Session s, Categorias categoria) {
+		s.save(categoria);
+		HibernateUtil.logger.info("Realizada inserccion de la categoria.");
+	}
+	
+	public static ArrayList<Categorias> getLista(Session s) {
+		String hql= "from Categorias";
+		Query query = s.createQuery(hql);
+		return (ArrayList<Categorias>) query.list();
+	}
+}
